acf93bf376126da6a9a065832f208ddf1b6e3b8e
Giovany Junior <cicerogiovany@gmail.com>
2026-05-08 13:56:00 -0300

    Add og-image

--- src/consts.ts
+++ src/consts.ts
@@ -39,5 +39,5 @@ export const SITE = {
   description:
     'Lead product designer com 9 anos de experiência. Portfólio de trabalhos selecionados em product design, design systems e IA.',
   url: 'https://giovanyux.github.io/portfolio',
-  ogImage: `${BASE}/og-image.jpg`,
+  ogImage: `${BASE}/og-image.png`,
 };
